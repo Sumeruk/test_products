106daf0dbc047c3a793edad1b94c898332ea9537
Sumeruk <zinchenkokostantin@yandex.ru>
2024-12-05 20:54:57 +0300

    maximize browser screen, set RunCucumber to default test

--- src/test/java/ru/Zinchenko/tests/BaseTest.java
+++ src/test/java/ru/Zinchenko/tests/BaseTest.java
@@ -2,8 +2,10 @@ package ru.Zinchenko.tests;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import ru.Zinchenko.properties.AppProperties;
 import ru.Zinchenko.utils.PropConst;
 
@@ -17,6 +19,8 @@ public class BaseTest {
     public static void initAll(){
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        driver.manage().window().maximize();
+
         driver.get(AppProperties.getProperty(PropConst.BASE_URL));
     }
 
